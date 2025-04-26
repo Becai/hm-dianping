dc580e2d3d873c86f4d6797ca1e1755f667ed35d
yxp <942645402@qq.com>
2025-04-26 10:56:52 +0800

    优惠券秒杀优化

--- src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -153,28 +153,28 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
     }
 
     private void handleVoucherOrder(VoucherOrder voucherOrder) {
-        Long userId = voucherOrder.getId();
+        // 集群环境下，分布式锁防止同一用户并发请求导致业务规则（一人一单）被破坏
+        Long userId = voucherOrder.getUserId();
         // 创建锁对象
         // SimpleRedisLock lock = new SimpleRedisLock("order:" + userId, stringRedisTemplate);
         RLock lock = redissonClient.getLock("lock:order:" + userId);
-        // 获取锁
+        // 尝试获取锁
         boolean isLock = lock.tryLock();
-        // 判断是否获取锁成功
         if(!isLock){
             // 获取锁失败，返回错误或重试
             log.error("不允许重复下单");
             return;
         }
         try {
-            // 获取代理对象（事务）
-            createVoucherOrder(voucherOrder);
+            // 获取代理对象，确保事务生效
+            IVoucherOrderService proxy = (IVoucherOrderService) AopContext.currentProxy();
+            proxy.createVoucherOrder(voucherOrder);
         } finally {
             // 释放锁
             lock.unlock();
         }
     }
 
-    IVoucherOrderService proxy;
     @Override
     public Result seckillVoucher(Long voucherId) {
         Long userId = UserHolder.getUser().getId();
@@ -191,27 +191,24 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
             // 2.1.不为0 ，代表没有购买资格
             return Result.fail(r == 1 ? "库存不足" : "不能重复下单");
         }
-        // 3.获取代理对象
-        proxy = (IVoucherOrderService) AopContext.currentProxy();
-        // 4.返回订单id
+        // 3.返回订单id
         return Result.ok(orderId);
     }
 
     @Transactional
     public void createVoucherOrder(VoucherOrder voucherOrder) {
-        // 5.一人一单
+        // 1.一人一单业务校验
         Long userId = voucherOrder.getUserId();
-
-        // 5.1.查询订单
+        // 1.1.查询订单
         int count = query().eq("user_id", userId).eq("voucher_id", voucherOrder.getVoucherId()).count();
-        // 5.2.判断是否存在
+        // 1.2.判断是否存在
         if (count > 0) {
             // 用户已经购买过了
             log.error("用户已经购买过一次！");
             return;
         }
 
-        // 6.扣减库存
+        // 2.乐观锁扣减库存
         boolean success = seckillVoucherService.update()
                 .setSql("stock = stock - 1") // set stock = stock - 1
                 .eq("voucher_id", voucherOrder.getVoucherId()).gt("stock", 0) // where id = ? and stock > 0
@@ -222,7 +219,7 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
             return;
         }
 
-        // 7.创建订单
+        // 3.持久化订单
         save(voucherOrder);
     }
 
